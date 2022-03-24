3e696194aa670da2d61f89ce66c52cd0cf95724c
StevenVallarsa <orionsjv@gmail.com>
2022-03-23 20:31:14 -0400

    added error checking on values but not types

--- src/com/sv/Main.java
+++ src/com/sv/Main.java
@@ -1,15 +1,13 @@
 package com.sv;
 
-import java.sql.SQLOutput;
-import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-//        monthlyPayment();
-        fizzBang(50);
+        monthlyMortgagePayment();
+//        fizzBang(50);
 
     }
 
@@ -29,35 +27,55 @@ public class Main {
         fizzBang(100);
     }
 
-    private static void monthlyPayment() {
+    private static void monthlyMortgagePayment() {
         Scanner scanner = new Scanner(System.in);
         byte MONTHS_IN_YEAR = 12;
         byte PERCENT = 100;
-        System.out.println("My Simple Mortgage Calculator");
+        System.out.println("~~~ My Simple Mortgage Calculator ~~~");
 
-        System.out.print("Principal ($): ");
-        int principal = scanner.nextInt();
+        int principal = 0;
+        while(true) {
+            System.out.print("\nPRINCIPAL ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000 ) {
+                break;
+            }
+            System.out.println("Enter an amount between $1000 and $1,000," +
+                    "000");
+        }
 
-        System.out.print("Interest (%): ");
-        double interest =
-                scanner.nextDouble()/PERCENT/MONTHS_IN_YEAR;
+        double monthlyInterestRate = 0;
+        while (true) {
+            System.out.print("\nAnnual Interest Rate (%): ");
+            double annualInterestRate = scanner.nextDouble();
+            if (annualInterestRate >= 0.25 && annualInterestRate <= 30) {
+                monthlyInterestRate = annualInterestRate/PERCENT/MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter an interest rate between 0.25% and " +
+                    "30%");
+        }
 
-        System.out.print("Length (Years): ");
-        int years = scanner.nextInt();
-        int months = years * MONTHS_IN_YEAR;
+        int years = 0;
+        int months = 0;
+        while(true) {
+            System.out.print("\nLength (Years): ");
+            years = scanner.nextInt();
+            if (years > 0 && years < 50) {
+                months = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter years between 1 and 50");
+        }
 
         double monthlyPayment =
-                principal * (interest * (Math.pow(1 + interest, months)) /
-                        (Math.pow(1 + interest, months) - 1));
+                principal * (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, months)) /
+                        (Math.pow(1 + monthlyInterestRate, months) - 1));
 
         NumberFormat df = NumberFormat.getCurrencyInstance();
         String amount = df.format(monthlyPayment);
 
-        System.out.println(amount);
+        System.out.println("\nYour monthly payment with be " + amount);
 
-        int income = 100_001;
-        boolean hasHighIncome = (income > 100_000);
-
-        System.out.println(hasHighIncome);
     }
 }
